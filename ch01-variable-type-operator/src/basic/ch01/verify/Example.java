9e828c4f5bd95ecb361e9e84740d8391fe98da7d
minjeong <minjeongkim0212@gmail.com>
2025-04-15 17:49:53 +0900

    문제 조금 품

---
+++ ch01-variable-type-operator/src/basic/ch01/verify/Example.java
@@ -0,0 +1,8 @@
+package basic.ch01.verify;
+
+public class Example {
+    public static void main(String[] args){
+        String exam = "개발자가 되기 위한 필수 개발 언어 Java";
+        System.out.println(exam);
+    }
+}
