2839fe7e9549734d80b61922edd4febc2df64af5
minjeong <minjeongkim0212@gmail.com>
2025-04-24 17:40:40 +0900

    0424_오후수업

--- ch07-java-base/src/lecture/date/Application1.java
+++ ch07-java-base/src/lecture/date/Application1.java
@@ -1,4 +1,12 @@
 package lecture.date;
 
+import java.util.Date;
+
 public class Application1 {
+    public static void main(String[] args) {
+        //Date 연도 설정 문제
+        Date legacyDate = new Date();
+        legacyDate.setYear(2025);
+        System.out.println("Date setYear(2025) -> 실제출력" + legacyDate); //1900이 + 되어서 출력됨
+    }
 }
--- ch07-java-base/src/lecture/wrapper/Application1.java
+++ ch07-java-base/src/lecture/wrapper/Application1.java
@@ -1,4 +1,37 @@
 package lecture.wrapper;
 
 public class Application1 {
+    public static void main(String[] args) {
+        /*
+         *  기본타입         래퍼클래스
+         *  byte          Byte
+         *  short         Short
+         *  int           Integer
+         *  long          Long
+         *  float         Float
+         *  double        Double
+         *  char          Character
+         *  boolean       Boolean
+         * */
+    /*
+    박싱과 언박싱
+    박싱: 기본 타입을 래퍼클래스의 인스턴스로 인스턴스화 하는 것
+    언박싱: 래퍼클래스의 인스턴스를 기본 타입으로 바꾸는 것
+     */
+
+        int intValue = 10;
+        //박싱
+//    Integer boxingNumber1 = new Integer(intValue);성능때문에 이렇게 안씀 더이상!!!
+        Integer boxingNumber1 = intValue;
+        Integer boxingNumber1a = Integer.valueOf(intValue);
+        int unBoxingNumber1 = boxingNumber1;
+
+        System.out.println(unBoxingNumber1);
+
+        Integer num1 = 10;
+        Integer num2 = 10;
+        int num3 = 20;
+        System.out.println("Integer와 Integer 비교: " + (num1 == num2));// true
+        System.out.println("int와 Integer 비교: " + (num1 == num3)); //false
+    }
 }
